123ecc33b091da2e00e7bc9d1c02407f27c87ecc
clakr <clarktolosa@gmail.com>
2023-05-28 10:29:29 +0800

    tablet done

--- uno.config.ts
+++ uno.config.ts
@@ -26,9 +26,13 @@ export default defineConfig({
       "black-russian": "#191826",
       coral: "#FA7453",
     },
+    breakpoints: {
+      tablet: "768px",
+      desktop: "1440px",
+    },
   },
   shortcuts: {
-    logo: "w-146 h-32",
+    logo: "w-146.09 h-32",
     button:
       "fw-700 fs-18 leading-32 -tracking-.18 flex items-center justify-center gap-y-7.92 pt-15 pb-14 rounded-12",
   },
@@ -143,6 +147,72 @@ export default defineConfig({
       /^bg-y-([\d.-]+)$/,
       ([, value]) => ({
         "background-position-y": convertToRem(value),
+      }),
+    ],
+    [
+      /^pr-([\d.-]+)$/,
+      ([, value]) => ({
+        "padding-right": convertToRem(value),
+      }),
+    ],
+    [
+      /^pl-([\d.-]+)$/,
+      ([, value]) => ({
+        "padding-left": convertToRem(value),
+      }),
+    ],
+    [
+      /^mr-([\d.-]+)$/,
+      ([, value]) => ({
+        "margin-right": convertToRem(value),
+      }),
+    ],
+    [
+      /^ml-([\d.-]+)$/,
+      ([, value]) => ({
+        "margin-left": convertToRem(value),
+      }),
+    ],
+    [
+      /^translate-x-([\d.-]+)$/,
+      ([, value]) => ({
+        "--un-translate-x": convertToRem(value),
+        transform: `translateX(var(--un-translate-x))
+        translateY(var(--un-translate-y)) translateZ(var(--un-translate-z))
+        rotate(var(--un-rotate)) rotateX(var(--un-rotate-x))
+        rotateY(var(--un-rotate-y)) rotateZ(var(--un-rotate-z))
+        skewX(var(--un-skew-x)) skewY(var(--un-skew-y)) scaleX(var(--un-scale-x))
+        scaleY(var(--un-scale-y)) scaleZ(var(--un-scale-z))`,
+      }),
+    ],
+    [
+      /^p-([\d.-]+)$/,
+      ([, value]) => ({
+        padding: convertToRem(value),
+      }),
+    ],
+    [
+      /^max-w-([\d.-]+)$/,
+      ([, value]) => ({
+        "max-width": convertToRem(value),
+      }),
+    ],
+    [
+      /^right-([\d.]+)$/,
+      ([, value]) => ({
+        right: convertToRem(value),
+      }),
+    ],
+    [
+      "bg-tablet",
+      {
+        "background-image": "url(/bg-main-tablet.png)",
+      },
+    ],
+    [
+      /^bg-x-([\d.-]+)$/,
+      ([, value]) => ({
+        "background-position-x": convertToRem(value),
       }),
     ],
   ],
